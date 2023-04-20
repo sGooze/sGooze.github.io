b444035f9509e4d51e4613097bfc691130435574
sGooze <sgooze@yandex.ru>
2023-04-21 00:27:35 +0300

    New Svelte-based version

---
+++ _app/immutable/chunks/2.34e142f0.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.06e26978.js";export{t as component};
---
+++ _app/immutable/chunks/3.050a0dde.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/en-page.svelte.73d301ed.js";export{t as component};
