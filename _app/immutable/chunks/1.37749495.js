9528fd4042cd588311bf9fd0e092a85e16c04827
sGooze <sgooze@yandex.ru>
2023-04-21 00:48:48 +0300

    Tiny CSS fix

---
+++ _app/immutable/chunks/1.37749495.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.a2db9a4e.js";export{t as component};
--- _app/immutable/chunks/1.6897ac1d.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.f037271c.js";export{t as component};
